b42ab1cb190f773e346bf4f5ce4fe27d7ab8a8fa
TheGuyFromIndia <31314482+TheGuyFromIndia@users.noreply.github.com>
2019-03-24 11:20:02 +0530

    created an intent

--- app/src/main/java/com/catchthe/android/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/catchthe/android/swoosh/LeagueActivity.kt
@@ -1,12 +1,19 @@
 package com.catchthe.android.swoosh
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 
 class LeagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun goToSkill(view: View) {
+        val skillactivity = Intent(this, skillActivity::class.java)
+        startActivity(skillactivity)
     }
 }
